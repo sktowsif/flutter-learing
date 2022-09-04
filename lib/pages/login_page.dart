56f14a55cd0cd4b62d78fbcd0d6b870b77a080e0
towsif-hossain <towsif.hossain@e9.co.uk>
2022-09-04 12:15:47 +0100

    day 8 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,81 +12,102 @@ class _LoginPageState extends State<LoginPage> {
   String _username = '';
   bool _isLoginDone = false;
 
+  final _loginFormKey = GlobalKey<FormState>();
+
+  _onLoginComplete(BuildContext context) async {
+    if (_loginFormKey.currentState?.validate() == true) {
+      setState(() {
+        _isLoginDone = true;
+      });
+      await Future.delayed(const Duration(seconds: 1));
+      Navigator.pushNamed(context, AppRoute.homeRoute);
+      setState(() {
+        _isLoginDone = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/images/img_login.png',
-                fit: BoxFit.cover,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Welcome $_username',
-                style:
-                    const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(labelText: 'Username'),
-                      onChanged: (value) {
-                        _username = value;
-                        setState(() {});
-                      },
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: const InputDecoration(labelText: 'Password'),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    InkWell(
-                      onTap: () async {
-                        setState(() {
-                          _isLoginDone = true;
-                        });
-                        await Future.delayed(const Duration(seconds: 1));
-                        Navigator.pushNamed(context, AppRoute.homeRoute);
-                      },
-                      child: AnimatedContainer(
-                        duration: const Duration(seconds: 1),
-                        height: 40,
-                        width: _isLoginDone ? 40 : 120,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.circular(_isLoginDone ? 40 : 4),
-                            color: Theme.of(context).primaryColor),
-                        child: _isLoginDone
-                            ? const Icon(
-                                Icons.done,
-                                color: Colors.white,
-                              )
-                            : const Text(
-                                'Login',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                      ),
-                    )
-                    // ElevatedButton(
-                    //     onPressed: () {
-                    //       Navigator.pushNamed(context, AppRoute.homeRoute);
-                    //     },
-                    //     child: const Text('Login'))
-                  ],
+          child: Form(
+            key: _loginFormKey,
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/img_login.png',
+                  fit: BoxFit.cover,
                 ),
-              )
-            ],
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Welcome $_username',
+                  style: const TextStyle(
+                      fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: 'Username'),
+                        onChanged: (value) {
+                          _username = value;
+                          setState(() {});
+                        },
+                        validator: (value) {
+                          if (value?.isEmpty == true) {
+                            return 'Username cannot be empty!';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                          obscureText: true,
+                          decoration:
+                              const InputDecoration(labelText: 'Password'),
+                          validator: (value) {
+                            if (value?.isEmpty == true) {
+                              return 'Password cannot be empty!';
+                            }
+                            return null;
+                          }),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Material(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius:
+                            BorderRadius.circular(_isLoginDone ? 40 : 4),
+                        child: InkWell(
+                          onTap: () => _onLoginComplete(context),
+                          child: AnimatedContainer(
+                            duration: const Duration(seconds: 1),
+                            height: 40,
+                            width: _isLoginDone ? 40 : 120,
+                            alignment: Alignment.center,
+                            child: _isLoginDone
+                                ? const Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                  )
+                                : const Text(
+                                    'Login',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
